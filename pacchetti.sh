783cd4cd60971f3d40f23a9ab2050c9a0b1348a7
GiacomoTorrigiani <101568206+GiacomoTorrigiani@users.noreply.github.com>
2023-02-27 19:38:25 +0100

    Update pacchetti.sh

--- pacchetti.sh
+++ pacchetti.sh
@@ -1,5 +1,5 @@
 cd /root/.node-red/node_modules
+npm install node-red-contrib-cast
 npm install node-red-contrib-alexa-remote2
 npm install node-red-contrib-fs-ops
-npm install node-red-contrib-cast
 npm install node-red-dashboard
