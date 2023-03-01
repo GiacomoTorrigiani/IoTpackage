b071fe5bc35aa33adac56510c9e23967dfe5a1da
GiacomoTorrigiani <101568206+GiacomoTorrigiani@users.noreply.github.com>
2023-03-01 14:44:32 +0100

    Update pacchetti.sh

--- pacchetti.sh
+++ pacchetti.sh
@@ -3,6 +3,6 @@ sleep 5
 npm install -g npm@latest
 sleep 5
 npm install node-red-contrib-cast
-npm install node-red-contrib-alexa-remote2
+npm install node-red-contrib-alexa-remote2-applestrudel
 npm install node-red-contrib-fs-ops
 npm install node-red-dashboard
