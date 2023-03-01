12eba304ed0ef20302d6370f8b4b41f37138be24
GiacomoTorrigiani <101568206+GiacomoTorrigiani@users.noreply.github.com>
2023-03-01 11:23:16 +0100

    Update pacchetti.sh

--- pacchetti.sh
+++ pacchetti.sh
@@ -1,4 +1,7 @@
 cd /root/.node-red/node_modules
+sleep 5
+npm install -g npm@latest
+sleep 5
 npm install node-red-contrib-cast
 npm install node-red-contrib-alexa-remote2
 npm install node-red-contrib-fs-ops
